d6e82859483902f5eb2cb91b7a58497b88217d12
Bram Borggreve <beeman@beeman.nl>
2023-04-11 12:47:39 +0100

    feat: add anonGetUserByIdentity method

--- libs/api/user/feature/src/lib/api-anon-user.resolver.ts
+++ libs/api/user/feature/src/lib/api-anon-user.resolver.ts
@@ -1,4 +1,5 @@
 import { Args, Query, Resolver } from '@nestjs/graphql'
+import { IdentityProvider } from '@pubkeyapp/api/identity/data-access'
 import { Invite } from '@pubkeyapp/api/invite/data-access'
 import { Page } from '@pubkeyapp/api/page/data-access'
 import { ApiPublicUserService, User } from '@pubkeyapp/api/user/data-access'
@@ -11,6 +12,11 @@ export class ApiAnonUserResolver {
   @Query(() => User, { nullable: true })
   anonGetUser(@Args('username') username: string) {
     return this.service.anonGetUser(username)
+  }
+
+  @Query(() => User, { nullable: true })
+  anonGetUserByIdentity(@Args('provider') provider: IdentityProvider, @Args('providerId') providerId: string) {
+    return this.service.getUserByIdentity(provider, providerId)
   }
 
   @Query(() => [User], { nullable: true })
